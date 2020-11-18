3b9649947c7745b3c2ebd901f879bbd2878f5494
eniallator <eniallatormc@gmail.com>
2020-11-18 14:28:18 +0000

    Formatted code, added auth.ini secret token configuration, put instructions in the README, added websockets to the dependency list

--- PAL.py
+++ PAL.py
@@ -5,126 +5,166 @@ Code runs and takes in questions from students
 It also awaits requests from the web from pal mentors, allowing them to answer questions. 
 """
 
+import os
 import discord
 from discord.ext import commands, tasks
 import asyncio
 import websockets
 import datetime
+import configparser
+
+
+# Environment variables can be configured on hosting services
+TOKEN = os.environ.get("DISCORD_TOKEN")
+
+if not TOKEN:
+    config = configparser.ConfigParser()
+    config.read("auth.ini")
+    try:
+        TOKEN = config.get("secret", "token")
+    except configparser.NoSectionError:
+        raise Exception("Could not find an auth.ini file with the bot token in it.")
+
 
 class Person:
-    #class binding each person
-    def __init__(self,name,channel):
-        self.time=datetime.datetime.now()
-        self.name=name
-        self.channel=channel
-        self.message=""
-    def setMessage(self,message):
-        self.message+=message
+    # class binding each person
+    def __init__(self, name, channel):
+        self.time = datetime.datetime.now()
+        self.name = name
+        self.channel = channel
+        self.message = ""
+
+    def setMessage(self, message):
+        self.message += message
+
 
 class PALbot(discord.Client):
-    #the main bot
-    #Stores unanswered questions
-    #Removes when answered in chat by PAL mentor
-    #Alerts PAL mentors to answer questions
-    async def on_ready(self,banned=["@PAL"]):
-        print('Logged on as', self.user)
-        self.questions=0
+    # the main bot
+    # Stores unanswered questions
+    # Removes when answered in chat by PAL mentor
+    # Alerts PAL mentors to answer questions
+    async def on_ready(self, banned=["@PAL"]):
+        print("Logged on as", self.user)
+        self.questions = 0
         self.change_status.start()
         self.check_calendar.start()
-        self.dataStruct={}
-        self.banned=banned
-        self.getQuestion="questions"
-        #bot-testing
-        self.setAnswer="pal-text"
-        self.channels={}
+        self.dataStruct = {}
+        self.banned = banned
+        self.getQuestion = "questions"
+        # bot-testing
+        self.setAnswer = "pal-text"
+        self.channels = {}
         for i in self.get_all_channels():
-                self.channels[str(i)]=i
+            self.channels[str(i)] = i
+
     async def on_message(self, message):
         # don't respond to ourselves
-        if message.author == self.user: #do not reply to self
+        if message.author == self.user:  # do not reply to self
             return
-        #log question
-        Pass=True
-        roles=[]
+        # log question
+        Pass = True
+        roles = []
         try:
-            roles=message.author.roles #gather the role of the user
+            roles = message.author.roles  # gather the role of the user
             for i in roles:
-                if str(i.name) in self.banned: #don't answer these people
-                    Pass=False
-        except AttributeError: #if DM
+                if str(i.name) in self.banned:  # don't answer these people
+                    Pass = False
+        except AttributeError:  # if DM
             pass
-        if str(message.channel) == self.getQuestion and (Pass): #if message in accepted channels from accepted people
-            chan=self.channels[self.setAnswer]
-            info=[chan,""] #store channel from and messages
-            potentialPerson=Person(message.author.name,chan)
-            message.content.replace(":::","/:").replace("@@@","/@")
+        if str(message.channel) == self.getQuestion and (
+            Pass
+        ):  # if message in accepted channels from accepted people
+            chan = self.channels[self.setAnswer]
+            info = [chan, ""]  # store channel from and messages
+            potentialPerson = Person(message.author.name, chan)
+            message.content.replace(":::", "/:").replace("@@@", "/@")
             if message.author.name not in self.dataStruct:
-                self.questions+=1 #increase message count for admin
-            person=self.dataStruct.get(message.author.name,[potentialPerson])[0]
-            person.setMessage(" "+message.content.replace(":::","/:").replace("@@@","/@"))
-            #add all messages from that user
-            self.dataStruct[message.author.name]=[person]
-            
+                self.questions += 1  # increase message count for admin
+            person = self.dataStruct.get(message.author.name, [potentialPerson])[0]
+            person.setMessage(
+                " " + message.content.replace(":::", "/:").replace("@@@", "/@")
+            )
+            # add all messages from that user
+            self.dataStruct[message.author.name] = [person]
+
         elif not Pass or str(message.channel) == self.setAnswer:
-            #check if this is a pal mentor answering a question
-            check=message.mentions #get all mentioned in the message
+            # check if this is a pal mentor answering a question
+            check = message.mentions  # get all mentioned in the message
             for i in check:
-                if i.name in self.dataStruct: #if person has asked question
-                    self.dataStruct.pop(i.name) #remove from data
-    @tasks.loop(seconds=60*60) #every hour check what is happening
-    async def check_calendar(self): #Notifications
+                if i.name in self.dataStruct:  # if person has asked question
+                    self.dataStruct.pop(i.name)  # remove from data
+
+    @tasks.loop(seconds=60 * 60)  # every hour check what is happening
+    async def check_calendar(self):  # Notifications
         pass
-    @tasks.loop(seconds=60*60*24) #every day check and message  
-    async def change_status(self): #Notifications
-        if self.questions>0:
-            for i in self.dataStruct: #loop through each un-answered item
-                t1=self.dataStruct[i][0].time #find the time it was sent
-                nowTime=datetime.datetime.now() #get the current time
-                timeSince=nowTime-t1
-                timeSince=int(timeSince.total_seconds())/60/60 #get in hours
-                if int(timeSince)>=24: #if sent more than 24 hours ago
-                    await self.messageChat("@PAL The following questions need answering ASAP: \n\n"+"@"+i+" "+self.dataStruct[i][0].message+"\n\n",self.channels[self.setAnswer])
-    async def messageChat(self,message,c): #message discord with given channel
+
+    @tasks.loop(seconds=60 * 60 * 24)  # every day check and message
+    async def change_status(self):  # Notifications
+        if self.questions > 0:
+            for i in self.dataStruct:  # loop through each un-answered item
+                t1 = self.dataStruct[i][0].time  # find the time it was sent
+                nowTime = datetime.datetime.now()  # get the current time
+                timeSince = nowTime - t1
+                timeSince = int(timeSince.total_seconds()) / 60 / 60  # get in hours
+                if int(timeSince) >= 24:  # if sent more than 24 hours ago
+                    await self.messageChat(
+                        "@PAL The following questions need answering ASAP: \n\n"
+                        + "@"
+                        + i
+                        + " "
+                        + self.dataStruct[i][0].message
+                        + "\n\n",
+                        self.channels[self.setAnswer],
+                    )
+
+    async def messageChat(self, message, c):  # message discord with given channel
         await c.send(message)
-client=PALbot()
+
+
+client = PALbot()
+
 
 async def PALmentor(websocket, path):
-    #Admin control loop
-    #Called when admin connects to the server
+    # Admin control loop
+    # Called when admin connects to the server
     print("admin")
     try:
         async for message in websocket:
-            #message structure: COMMAND:DataItem1:::DataItem2:::,....,:::DataItemN
+            # message structure: COMMAND:DataItem1:::DataItem2:::,....,:::DataItemN
             print(message)
-            if message[0:7] == "ANSWER:": #if answer code
-                message=message.replace("ANSWER:","") #remove from string
-                message=message.split(":::") #split at given identifier
-                #gather all information about the question
-                name=message[0]
-                person= client.dataStruct[name][0]
-                question=person.message
-                c=person.channel
-                client.dataStruct.pop(name) #remove from data
-                client.questions-=1 #decrease value
-                answer=message[1]
-                await client.messageChat("@"+name+"\n '"+question+"' \n\n"+answer,c)
-            elif message=="CONNECT":
-                string=""
+            if message[0:7] == "ANSWER:":  # if answer code
+                message = message.replace("ANSWER:", "")  # remove from string
+                message = message.split(":::")  # split at given identifier
+                # gather all information about the question
+                name = message[0]
+                person = client.dataStruct[name][0]
+                question = person.message
+                c = person.channel
+                client.dataStruct.pop(name)  # remove from data
+                client.questions -= 1  # decrease value
+                answer = message[1]
+                await client.messageChat(
+                    "@" + name + "\n '" + question + "' \n\n" + answer, c
+                )
+            elif message == "CONNECT":
+                string = ""
                 for i in client.dataStruct:
-                    string+=i+"@@@"+client.dataStruct[i][0].message+":::"
-                    if len(string)>500: #prevent websocket error
+                    string += i + "@@@" + client.dataStruct[i][0].message + ":::"
+                    if len(string) > 500:  # prevent websocket error
                         break
-                await websocket.send(string[:-3]) #send list of to add
-            elif message[0:7]=="DELETE:":
-                message=message.replace("DELETE:","") #remove from string
-                client.dataStruct.pop(message) #remove from data
+                await websocket.send(string[:-3])  # send list of to add
+            elif message[0:7] == "DELETE:":
+                message = message.replace("DELETE:", "")  # remove from string
+                client.dataStruct.pop(message)  # remove from data
     except websockets.exceptions.ConnectionClosedError:
-            print("Admin left")
-    
-#Run the PAL server side
+        print("Admin left")
+
+
+# Run the PAL server side
 asyncio.get_event_loop().run_until_complete(
-websockets.serve(PALmentor, port=4040)) #listen for pal mentors
+    websockets.serve(PALmentor, port=4040)
+)  # listen for pal mentors
 
 
-#Run the discord bot
-client.run(Test)
+# Run the discord bot
+client.run(TOKEN)
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Bot
+
 Project for developing a PAL bot
+
+## Setup
+
+- Firstly make sure you have `pipenv` installed, with the correct version of python (see `Pipfile` for the version)
+- Then navigate to this directory in your command line of choice, and then execute `pipenv install`
+- Once that's finished, make a copy of the `auth.ini.example` file and name it `auth.ini`
+- Then replace `token` value with your discord bot's secret token
+
+## Running The Bot
+
+- To run the bot, firstly navigate to this directory in your command line of choice, and then run `pipenv shell`
+  - This will open up a new CLI inside the virtual environment for this project
+- Then to run the bot, use `python PAL.py`
